9b05a841a4b4011768cd47cfea57c13ce221be1d
zhangshengshan <AlphaBetaGo@163.com>
2023-06-01 23:41:22 +0800

    commit

--- a.sql
+++ a.sql
@@ -1,4 +1,3 @@
-INSERT OVERWRITE TABLE ea_dg.rpt_datagov_hive_manta_coreconfig_matrix_da PARTITION ( date = '20230531' )
 SELECT '4852', 'EA', '4843'  , '一级监控', NULL , '表分区无数据' ,'4843'/'4852', '整体' , NULL
 UNION ALL 
 SELECT '2905', 'EA', '2836'  , '二级监控', NULL , '枚举值异常'   ,'2836'/'2905', '整体' , NULL
--- source/_posts/HelloWorld.md
+++ source/_posts/HelloWorld.md
@@ -7,4 +7,8 @@ tags: A
 
 # HELLO WORLD  
 
+
 https://www.youtube.com/watch?v=dq_mBJj1sjg
+
+间奏部分参照
+https://www.bilibili.com/video/BV1TQ4y1d7J1/?spm_id_from=333.337.search-card.all.click&vd_source=c3c2e2f79b08e119e00c69923ed95cbb
